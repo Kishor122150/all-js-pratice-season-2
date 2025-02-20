57e1ce8309219502e822bc0cd0e85fc46d78ac02
Kishor122150 <kishorraut122150@gmail.com>
2025-02-20 21:14:50 +0545

    new js added

---
+++ all-js-pratice-season-2.js
@@ -0,0 +1,90 @@
+// operator in js 
+//artimetic operator
+// let a=10;
+// let b=27;
+// sum=a+b;
+// diff=a-b;
+// mul=a*b;
+// divv=a/b;
+// console.log("sum of a+b is",sum);
+// console.log("multiply of a*b is",mul);
+// console.log("subtraction of a-b is",diff);
+// console.log("division of a/b is",divv);
+
+// let a=5;
+// let b=10;
+// a+=5;
+// console.log(a)
+// a-=2;
+// console.log(a)
+
+// comparasion operator 
+// let a=5;
+// let b=3;
+// console.log("a==b is:",a==b) //false
+// console.log("a!=b is:",a!=b) //true
+// console.log(a>b)
+// console.log(a<b)
+// console.log(a<=b)
+// console.log(a>=b)
+
+//logical operator
+// let a=20;
+// let b=27;
+// let cond1=a<b;
+// let cond2=a!=20;
+// console.log("cond1 and cond2 is",cond1 && cond2) //false
+// console.log("cond1 or cond2 is ", cond1 || cond2) //true
+// console.log(!cond1) //true=>false
+
+//conditional statement
+
+//if
+// const mode="white";
+// if(mode=="dark"){
+//     console.log("color=black") ;
+
+// }else if (mode=="white"){
+//     console.log("color=white") ;
+//  }
+ 
+//if else
+// let age=prompt("enter your age")
+// if(age>=18){
+//     console.log(alert("you are a voter"))
+
+// }else{
+//     console.log(("you are not"))
+// }
+
+//even or odd
+// let number=prompt("enter a number")
+// if(number%2===0){
+//     console.log("the given number is even" )
+// }else{
+//     console.log("the given number is odd")
+// }
+
+// if-elseif
+// let mode="white";
+// let color;
+// if(mode==="dark"){
+//     document.write(color="black")
+// }else if(mode==="white"){
+//     document.write(color="white")
+// }else{
+//     document.write(color="red")
+
+// }
+
+// ternary operator
+// syntax:condition?true output:false output;
+// let age=20;
+// let result= age>=18?"adult":"not adult";
+// console.log(result)
+
+// let age=18;
+// age>=18?console.log("adult"):console.log("not adult");
+
+let name=prompt("hello")
+console.log(name)
