40295f0e4aed8b2f20e281dd3dcbe4c63cbcc525
Kishor122150 <kishorraut122150@gmail.com>
2025-02-21 08:36:35 +0545

    new pratices added

--- all-js-pratice-season-2.js
+++ all-js-pratice-season-2.js
@@ -86,5 +86,137 @@
 // let age=18;
 // age>=18?console.log("adult"):console.log("not adult");
 
-let name=prompt("hello")
-console.log(name)
+// let name=prompt("hello")
+// console.log(name)
+
+//wap where the given number is multiply by or not 
+// let num=prompt("enter the number")
+// if(num%5===0){
+//     console.log(num,"is multiple of 5")
+// }else{
+//     console.log(num,"is not multiples of 5")
+// }
+
+//wap which can gives grades to student according to score
+// let score=prompt("enter the score between (1-100")
+// let grade;
+// if(score>=90 && score<=100){
+//     grade="a"
+// }
+// else if(score>=70 && score<=89){
+//     grade="b";
+// }
+// else if(score>=60 && score<=69){
+//     grade="c";
+// }
+// else if(score>=50 && score<=59){
+//     grade="d"
+// }
+// else if(score>=0 && score<=49){
+//     grade="f"
+// }
+// console.log("your grade is ",grade)
+
+// loops in js 
+// priint name 10 times 
+// for loop
+// for(let i=1;i<=10;i++){
+//     console.log(i+".hello world");
+// }
+// for(let count=1;count<=5;count++){
+//     console.log("kishor")
+// }
+// calculate sum of 1 to 5 
+// let sum =0;
+// for(let i=1;i<=5;i++){
+//     sum=sum+i;
+//     console.log(i)
+// }
+// console.log(sum)
+
+// multiply of 1 to 100
+// let multiply=1;
+// let sum=0;
+// for(let i=1;i<=5;i++){
+//     multiply=multiply*i;
+// }
+// console.log(multiply)
+ 
+//while-loop
+// let i=1;
+// while(i<=10){
+//     console,console.log(i);
+//     i++;
+    
+// }
+
+//do-while loop
+// let i=1;
+// do{
+//     console.log("kishor");
+//     i++;
+// }while(i<=10)
+
+// print number from 1 to 10
+// let i=1;
+// do{
+//     console.log(i)
+//     i++;
+// } while(i<=10)
+
+// for in loop
+// let student={
+//     name:"Kishor",
+//     age:20,
+//     gpa:7.5,
+//     ispass:true,
+// };
+// for(let i in student){
+//     console.log(i)
+// }
+
+
+// print all the even number foem 1 to 100;
+// for(num=1;num<=100;num++){
+//     if(num %2===0){
+//         console.log(num)
+//     }
+// }
+
+//created a game where you can assk user to start and gusssing the numbers untill its correct
+// let gamenum=27;
+// let usernum=prompt("enter the number");
+// while(usernum!=gamenum){
+//     usernum=prompt("you enter wrong number please enter again");
+// }
+// console.log(alert("cnogratulations"))
+// or
+// let gamenum=20;
+// let usernum=prompt("enter the number")
+// if(gamenum===usernum){
+//     alert("correct number")
+// }else{
+//     alert("wrong number guess again")
+// }
+
+// or 
+
+//string in js
+// let name="kishor";
+// console.log(name)
+
+// string indices
+// let name="kishor";
+// console.log(name[0])
+
+// templated literals in js 
+// let str=`i am templated literals`
+// console.log(str)
+// console.log(typeof(str))
+
+let obj={
+    name:"kishor",
+    age:20,
+};
+let output=`my name is ${obj.name} and i am  ${obj.age} years old`
+console.log(output)
